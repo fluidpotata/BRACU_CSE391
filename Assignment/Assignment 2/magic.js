e5fdbce3d5b7e094d8ecffeb54bf8ec7be8b50dd
Alif <md.ibrahim.alif@g.bracu.ac.bd>
2024-12-02 21:41:13 +0600

    Assignment 2 done

---
+++ Assignment/Assignment 2/magic.js
@@ -0,0 +1,78 @@
+const textArea = document.getElementById('formDisplay');
+let isCapitalized = false;
+
+function clearAll() {
+    textArea.value = '';
+}
+
+function toggleCapitalize() {
+    if (isCapitalized) {
+        textArea.value = textArea.value.toLowerCase();
+        isCapitalized = false;
+    } else {
+        textArea.value = textArea.value.toUpperCase();
+        isCapitalized = true;
+    }
+}
+
+function sortLines() {
+    const lines = textArea.value.split('\n');
+    const nonEmptyLines = [];
+    for (let i = 0; i < lines.length; i++) {
+        if (lines[i].trim() !== '') {
+            nonEmptyLines.push(lines[i]);
+        }
+    }
+    nonEmptyLines.sort();
+    textArea.value = nonEmptyLines.join('\n');
+}
+
+function reverseLines() {
+    const lines = textArea.value.split('\n');
+    const reversedLines = [];
+    for (let i = 0; i < lines.length; i++) {
+        reversedLines.push(lines[i].split('').reverse().join(''));
+    }
+    textArea.value = reversedLines.join('\n');
+}
+
+function stripBlank() {
+    const lines = textArea.value.split('\n');
+    const strippedLines = [];
+    for (let i = 0; i < lines.length; i++) {
+        const trimmedLine = lines[i].trim();
+        if (trimmedLine !== '') {
+            strippedLines.push(trimmedLine);
+        }
+    }
+    textArea.value = strippedLines.join('\n');
+}
+
+function addNumbers() {
+    const lines = textArea.value.split('\n');
+    const numberedLines = [];
+    for (let i = 0; i < lines.length; i++) {
+        const trimmedLine = lines[i].trim();
+        if (trimmedLine !== '') {
+            numberedLines.push((i + 1) + '. ' + trimmedLine);
+        }
+    }
+    textArea.value = numberedLines.join('\n');
+}
+
+function shuffleLines() {
+    const lines = textArea.value.split('\n');
+    const nonEmptyLines = [];
+    for (let i = 0; i < lines.length; i++) {
+        if (lines[i].trim() !== '') {
+            nonEmptyLines.push(lines[i]);
+        }
+    }
+    for (let i = nonEmptyLines.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        const temp = nonEmptyLines[i];
+        nonEmptyLines[i] = nonEmptyLines[j];
+        nonEmptyLines[j] = temp;
+    }
+    textArea.value = nonEmptyLines.join('\n');
+}
