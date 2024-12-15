5b300c05bb3f18f2ea1c6ef34e111ef0c57d00f3
Alif <md.ibrahim.alif@g.bracu.ac.bd>
2024-12-16 02:01:49 +0600

    ADD ass3 database helper

---
+++ Assignment/Assignment 3/helper.py
@@ -0,0 +1,35 @@
+def ifAdmin():
+    # database connection
+    return # result
+
+
+def clientRegister():
+    # A client visiting to the web application should be able to entry their Name, Address, Phone
+    return 
+
+
+def takeAppointment():
+    # A client should be able to entry their Car License number, Car Engine Number, appointment date and can select their desire mechanic
+    return 
+
+def ifCarExists():
+    # database connection
+    return # result
+
+def isMechanicAvailable():
+    # each mechanic is allowed to get assign on maximum 4 active cars(clients) everyday
+    # if the mechanic is occupied with maximum no. of appointment the system should notify client otherwise it will approve the appointment.
+
+    return # result
+
+def showAllMechanics():
+    # The list of the mechanic should show how many free places are available for each mechanic
+    return # result
+
+def ifAppointmentExists():
+    # After a client makes data submission, it should check whether the client has already taken any appointment from any mechanic on certain date or not
+    # If not, then the client is allowed to take an appointment
+    # otherwise s/he will be notifying that he has already taken an appointment on that specific date.
+    return
+
+
