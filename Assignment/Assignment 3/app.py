465c66614854bdc9dbcde30f71e5d19d05e5d64c
Md Ibrahim Alif <md.ibrahim.alif@g.bracu.ac.bd>
2024-12-17 01:32:50 +0600

    Add Customer Features, admin dash view

--- Assignment/Assignment 3/app.py
+++ Assignment/Assignment 3/app.py
@@ -1,3 +1,4 @@
+import re
 from flask import Flask, redirect,render_template,request, session, url_for
 from helper import *
 
@@ -6,8 +7,9 @@ app.config["SECRET_KEY"] = "any random string"
 
 
 @app.route('/')
-def hello():
+def index():
     return render_template('index.html')
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -16,26 +18,98 @@ def login():
         password = request.form['password']
         if isAuthenticated(username, password):
             session['username'] = username
-            response = redirect(url_for('dashboard'))
-            response.set_cookie('user_id', username)
-            return response
-    return render_template('login.html')
+            if ifAdmin(username):
+                return redirect(url_for('admin'))
+            return redirect(url_for('dashboard'))
+        else:
+            return render_template('login.html', error="Invalid username or password")
+    elif request.method == 'GET':
+        try:
+            if session['username']:
+                return render_template('dashboard.html')
+        except:
+            return render_template('login.html')
 
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    return render_template('signup.html')
+    if request.method == 'GET':
+        if session['username']:
+            return render_template('dashboard.html')
+        else:
+            return render_template('signup.html')
+    elif request.method == 'POST':
+        name = request.form['name']
+        username = request.form['username']
+        address = request.form['address']
+        phone = request.form['phone']
+        password = request.form['password']
+        cpassword = request.form['confirm_password']
+        if password != cpassword:
+            return render_template('signup.html', error="Password doesn't match")   
+        if ifUserExists(username):
+            return render_template('signup.html', error="Username taken! Try another username.")
+        clientRegister(name, address, phone, username, password)
+        return redirect(url_for('login'))
 
 
-@app.route('/dashboard')
+
+@app.route('/admin')
+def admin():
+    tdata = getClientAppointments()
+    data = []
+    for i in tdata:
+        data.append((getClientName(i[1]), getMechanicName(i[2]), i[3], i[4], i[5]))
+    return render_template('admin.html', data=data)
+
+
+@app.route('/dashboard', methods=['GET', 'POST'])
 def dashboard():
-    return render_template('dashboard.html', data=getClientAppointments(session['username']))
+    if request.method == 'POST':
+        data = {}
+        data['carLicenseNumber'] = request.form['carLicenseNumber']
+        data['carEngineNumber'] = request.form['carEngineNumber']
+        data['carAppointmentDate'] = request.form['carAppointmentDate']
+        # return render_template('appointment.html', carLicenseNumber=carLicenseNumber, carEngineNumber=carEngineNumber, carAppointmentDate=carAppointmentDate)
+        session['mechanic'] = data
+        return redirect(url_for('appointment'))
+    elif request.method == 'GET':
+        try:
+            if session['username']:
+                return render_template('dashboard.html')
+        except:
+            return redirect(url_for('login'))
 
 
-@app.route('/appointment')
+
+@app.route('/appointment', methods=['GET', 'POST'])
 def appointment():
-    return render_template('appointment.html')
+    if request.method == 'POST':
+        appdata = session['mechanic']   
+        carLicenseNumber, carEngineNumber, carAppointmentDate = appdata['carLicenseNumber'], appdata['carEngineNumber'], appdata['carAppointmentDate']
+        mechanic = request.form['mechanic']
+        if ifAppointmentExists(carLicenseNumber, carEngineNumber, carAppointmentDate, mechanic):
+            return render_template('appointment.html', error="You have already taken an appointment")
+        if isMechanicAvailable(carAppointmentDate, mechanic):
+            takeAppointment(session['username'], carLicenseNumber, carEngineNumber, carAppointmentDate, mechanic)
+            return redirect(url_for('dashboard'))
+        else:
+            return render_template('appointment.html', error="Mechanic is not available")
+    elif request.method == 'GET':
+        try:
+            if session['username']:
+                data = session.get('mechanic')
+                date = data['carAppointmentDate']
+                mech = showAllMechanics(date)
+                return render_template('appointment.html', mechanics=mech)
+        except:
+            return redirect(url_for('login'))
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
--- Assignment/Assignment 3/helper.py
+++ Assignment/Assignment 3/helper.py
@@ -5,15 +5,19 @@ def dbConnect():
     return sqlite3.connect('main.db')
 
 
-def ifAdmin():
-    # database connection
-    return # result
+def ifAdmin(username):
+    connection = dbConnect()
+    cursor = connection.cursor()
+    cursor.execute(f"SELECT role FROM Users WHERE username='{username}'")
+    result = cursor.fetchall()
+    connection.close()
+    return result[0][0] == 'Admin'
 
 
 def isAuthenticated(username, password):
     connection = dbConnect()
     cursor = connection.cursor()
-    cursor.execute(f"SELECT * FROM Users WHERE username={username} AND password={password}")
+    cursor.execute(f"SELECT * FROM Users WHERE username='{username}' AND password='{password}'")
     result = cursor.fetchall()
     connection.close()
     if len(result)>0:
@@ -21,37 +25,99 @@ def isAuthenticated(username, password):
     return False
 
 
-def clientRegister():
+def clientRegister(name, address, phone, username, password):
     # A client visiting to the web application should be able to entry their Name, Address, Phone
+    connection = dbConnect()
+    cursor = connection.cursor()
+    cursor.execute(f"INSERT INTO Users(name, address, phone, username, password) VALUES('{name}', '{address}', '{phone}', '{username}', '{password}')")
+    connection.commit()
+    connection.close()
     return 
 
 
-def takeAppointment():
+def takeAppointment(username, carLicenseNumber, carEngineNumber, carAppointmentDate, mechanic):
     # A client should be able to entry their Car License number, Car Engine Number, appointment date and can select their desire mechanic
-    return 
+    connection = dbConnect()
+    cursor = connection.cursor()
+    cursor.execute(f"SELECT id from Users where username='{username}'")
+    userid = cursor.fetchall()[0][0]
+    cursor.execute(f"INSERT INTO Appointments(carLicenseNumber, carEngineNumber, carAppointmentDate, mechanicID, userID) VALUES('{carLicenseNumber}', '{carEngineNumber}', '{carAppointmentDate}', '{mechanic}', '{userid}')")
+    connection.commit()
+    connection.close()
+    return
 
-def isMechanicAvailable():
+def isMechanicAvailable(carAppointmentDate, mechanic):
     # each mechanic is allowed to get assign on maximum 4 active cars(clients) everyday
     # if the mechanic is occupied with maximum no. of appointment the system should notify client otherwise it will approve the appointment.
+    connection = dbConnect()
+    cursor = connection.cursor()
+    cursor.execute(f"SELECT * FROM Appointments WHERE carAppointmentDate='{carAppointmentDate}' AND mechanicID='{mechanic}'")
+    result = cursor.fetchall()
+    connection.close()
+    if len(result)<4:
+        return True
+    return False
 
-    return # result
-
-def showAllMechanics():
+def showAllMechanics(date):
     # The list of the mechanic should show how many free places are available for each mechanic
-    return # result
+    connection = dbConnect()
+    cursor = connection.cursor()
+    cursor.execute(f"""
+        SELECT m.*, COUNT(a.id) as appointments_today
+        FROM mechanic m
+        LEFT JOIN Appointments a ON m.id = a.mechanicID AND DATE(a.carAppointmentDate) = DATE('{date}')
+        GROUP BY m.id
+    """)
+    result = cursor.fetchall()
+    connection.close()
+    return result
 
-def ifAppointmentExists():
+def ifAppointmentExists(carLicenseNumber, carEngineNumber, carAppointmentDate, mechanic):
     # After a client makes data submission, it should check whether the client has already taken any appointment from any mechanic on certain date or not
     # If not, then the client is allowed to take an appointment
     # otherwise s/he will be notifying that he has already taken an appointment on that specific date.
-    return
-
-def getClientAppointments(username):
     connection = dbConnect()
     cursor = connection.cursor()
-    cursor.execute(f"SELECT * FROM Appointments WHERE username={username}")
+    cursor.execute(f"SELECT * FROM Appointments WHERE carLicenseNumber='{carLicenseNumber}' AND carEngineNumber='{carEngineNumber}' AND carAppointmentDate='{carAppointmentDate}' AND mechanicID='{mechanic}'")
+    result = cursor.fetchall()
+    connection.close()
+    if len(result)>0:
+        return True
+    return False
+
+def getClientAppointments():
+    connection = dbConnect()
+    cursor = connection.cursor()
+    cursor.execute(f"SELECT * FROM Appointments")
     result = cursor.fetchall()
     connection.close()
     return result
 
 
+def ifUserExists(username):
+    connection = dbConnect()
+    cursor = connection.cursor()
+    cursor.execute(f"SELECT * FROM Users WHERE username='{username}'")
+    result = cursor.fetchall()
+    connection.close()
+    if len(result)>0:
+        return True
+    return False
+
+
+def getClientName(userid):
+    connection = dbConnect()
+    cursor = connection.cursor()
+    cursor.execute(f"SELECT name FROM Users WHERE id='{userid}'")
+    result = cursor.fetchall()
+    connection.close()
+    return result[0][0]
+
+
+def getMechanicName(mechaID):
+    connection = dbConnect()
+    cursor = connection.cursor()
+    cursor.execute(f"SELECT first_name, last_name FROM mechanic WHERE id='{mechaID}'")
+    result = cursor.fetchall()
+    connection.close()
+    return result[0][0]+" "+result[0][1]
